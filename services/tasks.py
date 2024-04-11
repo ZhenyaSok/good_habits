e6373795f8489e1bbd24f1d0152e82b0827b9aab
kapit <kapitan.kub@gmail.com>
2024-04-11 17:24:07 +0500

    Периодическая задача готова(2 варианта)

--- services/tasks.py
+++ services/tasks.py
@@ -1,19 +1,41 @@
-# from celery import shared_task
-# from datetime import datetime, timedelta
+
 from config import settings
-# from habits.models import Habit
-# from services.send_tg_bot import create_message, send_message
 import telebot
-# import os
 from celery import shared_task
 from datetime import datetime, timedelta, timezone
 from habits.models import Habit
-
+from services.send_tg_bot import TelegramBot
 
 API_KEY = settings.TELEGRAM_API_TOKEN
 
+
+'''Вариант 1: Рассылка уведомление происходит при помощи class TelegramBot (в файле send_tg_bot)'''
+
 @shared_task
 def your_habits():
+    now = datetime.now(tz=timezone.utc)
+    bot = TelegramBot()
+    habits = Habit.objects.filter(time__lte=now)
+
+    for habit in habits:
+        message = (f'В {habit.time.strftime("%H:%M")} '
+                   f'вы планировали {habit.action} в {habit.place}')
+
+        try:
+            bot.send_message(text=message)
+            habit.time += timedelta(days=habit.periodicity)
+            break
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            habit.save()
+
+
+"""Вариант 2: при задействовании этой функции в периодическую задача,
+рассылка будет происходить при помощи готовой библиотеки для рассылки в телеграмм (telebot)"""
+def send_habits():
     now = datetime.now(tz=timezone.utc)
     bot = telebot.TeleBot(API_KEY)
     habits = Habit.objects.filter(time__lte=now)
@@ -34,53 +56,3 @@ def your_habits():
 
         finally:
             habit.save()
-
-
-# @shared_task
-# def your_habits():
-#     now = datetime.now(tz=timezone.utc)
-#     bot = telebot.TeleBot(API_KEY)
-#     habits = Habit.objects.filter(time__lte=now)
-#
-#     for habit in habits:
-#         chat_id = habit.owner.chat_id
-#         message = (f'В {habit.time.strftime("%H:%M")} '
-#                    f'вы планировали сделать {habit.action} в {habit.place}')
-#
-#         try:
-#             bot.send_message(chat_id=chat_id,
-#                                         text=message)
-#             habit.time += timedelta(days=habit.periodicity)
-#             break
-#
-#         except Exception as e:
-#             print(e)
-#
-#         finally:
-#             habit.save()
-
-
-# def check_and_send():
-#     bot_token = settings.TELEGRAM_API_TOKEN
-#     """
-#         Отправляет напоминания о привычках пользователям через Telegram.
-#     """
-#     good_habit = Habit.objects.filter(sign_of_pleasant_habit=False)
-#     now_time = datetime.now().time()
-#     now_date = datetime.now().today()
-#
-#     for habit in good_habit:
-#         if not habit.next_date:
-#             if habit.time < now_time:
-#
-#                 chat_id, message = create_message(habit)
-#                 send_message(chat_id, message, bot_token)
-#                 habit.next_date = now_date + timedelta(days=habit.periodicity)
-#                 habit.save()
-#
-#             elif habit.next_date <= now_date:
-#
-#                 chat_id, message = create_message(habit)
-#                 send_message(chat_id, message, bot_token)
-#                 habit.next_date = now_date + timedelta(days=habit.periodicity)
-#                 habit.save()
